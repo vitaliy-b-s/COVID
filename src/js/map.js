a0da6327ad1916122b099a337c6d626b07d377e8
Vitaliy <butaji9i54@gmail.com>
2021-02-05 00:23:33 +0300

    project done

--- src/js/map.js
+++ src/js/map.js
@@ -1,10 +1,18 @@
-import { sortByNumberOfCases, buildStatisticsTable } from "./util";
+import { buildStatisticsTable, sortByNumberOfCases } from "./util";
+
 const MAP_LAYER =
   "https://tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey=59a66f1f84374967b29c9edc3526fdaf";
-const maxSize = 60;
-const minSize = 10;
+const MIN_MARKER_SIZE = 10;
+const MAX_MARKER_SIZE = 60;
+
+const NUMBER_FORMAT = new Intl.NumberFormat("ru-RU");
 
 export function initMap(covidData, populationData, geoData) {
+  const map = createMap();
+  addMarkersToMap(map, covidData, populationData, geoData);
+}
+
+function createMap() {
   const mapOptions = {
     center: [0.0, 0.0],
     zoom: 2,
@@ -14,9 +22,17 @@ export function initMap(covidData, populationData, geoData) {
   const layer = new L.TileLayer(MAP_LAYER, {
     noWrap: true,
   });
-  const sortedCasesArray = sortByNumberOfCases(covidData);
-
   map.addLayer(layer);
+  return map;
+}
+
+function addMarkersToMap(map, covidData, populationData, geoData) {
+  const sortedCases = sortByNumberOfCases(covidData);
+  const maxOfTotalConfirmed = sortedCases[0].TotalConfirmed;
+  const minOfTotalConfirmed = sortedCases[sortedCases.length - 1].TotalConfirmed;
+  const step = Math.trunc(
+    (maxOfTotalConfirmed - minOfTotalConfirmed) / (MAX_MARKER_SIZE - MIN_MARKER_SIZE)
+  );
 
   covidData.forEach(elem => {
     const geo = geoData.find(x => x.name === elem.Country);
@@ -26,58 +42,57 @@ export function initMap(covidData, populationData, geoData) {
       return;
     }
 
-    const calculatedSize = calculateSizeOfmarker(elem, sortedCasesArray);
+    const markerSize = calculateExtraSize(elem, step);
 
-    const iconOptions = {
-      iconUrl: "src/assets/img/marker.png",
-      iconSize: [calculatedSize, calculatedSize],
-    };
+    const markerDescription = buildMarkerDescription(elem, population);
 
-    const customIcon = L.icon(iconOptions);
+    const markerOptions = buildMarkerOptions(markerSize, elem);
 
-    const markerDescription = `   Country: ${elem.Country},
-                Population: ${new Intl.NumberFormat("ru-RU").format(
-                  population.population
-                )},
-                Total confirmed: ${new Intl.NumberFormat("ru-RU").format(
-                  elem.TotalConfirmed
-                )},
-                Total deaths: ${new Intl.NumberFormat("ru-RU").format(
-                  elem.TotalDeaths
-                )},
-        `;
-
-    const markerOptions = {
-      title: elem.Country,
-      clickable: true,
-      icon: customIcon,
-    };
-
-    if (geo.latlng === false) {
-      return;
-    } else {
-      const marker = L.marker(geo.latlng, markerOptions);
-      marker.bindPopup(markerDescription).openPopup();
-      marker.addTo(map);
-      marker.addEventListener("click", event => {
-        const country = covidData.find(
-          x => x.Country === event.target.options.title
-        );
-        document.querySelector(".statistics__title").innerHTML =
-          event.target.options.title;
-        document.querySelector(".units-change__button").style = "display: flex";
-        buildStatisticsTable(country);
-      });
-    }
+    createMarker(map, geo, markerDescription, markerOptions);
   });
 }
 
-function calculateSizeOfmarker(covidData, sortedArray) {
-  const maxCases = sortedArray[0].TotalConfirmed;
-  const minCases = sortedArray[sortedArray.length - 1].TotalConfirmed;
-  const step = Math.trunc((maxCases - minCases) / (maxSize - minSize));
-  const extraSize = Math.trunc(covidData.TotalConfirmed / step);
-  const calculatedSize = minSize + extraSize;
+function calculateExtraSize(country, step) {
+  const extraSize = Math.trunc(country.TotalConfirmed / step);
+  return MIN_MARKER_SIZE + extraSize;
+}
 
-  return calculatedSize;
+function buildMarkerDescription(country, population) {
+  return `Country: ${country.Country},
+    Population: ${NUMBER_FORMAT.format(population.population)},
+    Total confirmed: ${NUMBER_FORMAT.format(country.TotalConfirmed)},
+    Total deaths: ${NUMBER_FORMAT.format(country.TotalDeaths)}
+`;
+}
+
+function buildMarkerOptions(markerSize, country) {
+  const iconOptions = {
+    iconUrl: "src/assets/img/marker.png",
+    iconSize: [markerSize, markerSize],
+  };
+
+  const customIcon = L.icon(iconOptions);
+
+  return {
+    title: country.Country,
+    clickable: true,
+    icon: customIcon,
+  };
+}
+
+function createMarker(map, geo, markerDescription, markerOptions) {
+  if (geo.latlng === undefined) {
+    return;
+  } else {
+    const marker = L.marker(geo.latlng, markerOptions);
+
+    marker.bindPopup(markerDescription).openPopup();
+    marker.addTo(map);
+
+    marker.addEventListener("click", event => {
+      document.querySelector(".statistics__title").innerHTML = event.target.options.title;
+      document.querySelector(".units-change__button").style = "display: flex";
+      buildStatisticsTable(country);
+    });
+  }
 }
